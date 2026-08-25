e539109961ba52fb3fffd40708ca2f86b403dd60
mmitacc <manuelmitacc@hotmail.com>
2026-08-25 14:17:38 -0500

    add documentacion swagger

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,23 @@ import type { Request, Response } from "express";
 import libroRouter from './routers/libro.router.js';
 import socioRouter from './routers/socios_route.js';
 import prestamoRouter from './routers/prestamos.routes.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'node:fs';
+import path from 'node:path';
 
 dotenv.config();
 const app = expres();
 const PORT = process.env.PORT || 3000;
+
+// Middleware swagger
+const swaggerFilePath = path.resolve('./src/swagger-output.json');
+if (fs.existsSync(swaggerFilePath)) {
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  console.log('Archivo swagger cargado, exitosamente!');
+} else {
+  console.log('Archivo swagger json, no encontrado');
+}
 
 app.use(cors());
 
